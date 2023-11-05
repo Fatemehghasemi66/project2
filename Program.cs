00e372f6ab3ee334cef1ab3e756f37b18b2bc426
Fatemehghasemi66 <f_s_ghasemi@yahoo.com>
2023-11-05 18:54:54 +0330

    help

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace project2
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace project2
 {
     internal class Program
     {
@@ -6,39 +8,71 @@
         {
             Console.WriteLine("************************************************************");
         }
+            public static void FibonacciRecursive(int len)
+            {
+                FibonacciRecursiveTemp(0, 1, 1, len);
+            }
+        public static void ّFibonacciRecursiveTemp (int a,int b, int counter, int len)
+        {
+            if (counter <= len)
+            {
+                Console.WriteLine(a);
+                FibonacciRecursiveTemp(b, a + b, counter + 1, len);
+
+            }
+           
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hellocsharp!");
             Divider();
+            //Console.WriteLine("please enter your gender? (m,F)");
+            //string genderSTR = Console.ReadLine();
+            //Console.WriteLine("pleas enter your age");
+            //string ageSTR= Console.ReadLine();
+            //int age = Convert.ToInt32(ageSTR);
+            //if (genderSTR == "F" &&  age <= 30) 
+            //{
+            //    Console.WriteLine("registeration");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("reject");
+            //}
+            Divider();
             Console.WriteLine("please enter your gender? (m,F)");
             string genderSTR = Console.ReadLine();
             Console.WriteLine("pleas enter your age");
-            string ageSTR= Console.ReadLine();
+            string ageSTR = Console.ReadLine();
             int age = Convert.ToInt32(ageSTR);
-            if (genderSTR == "F" &&  age <= 30) 
+            string massage = genderSTR == "F" && age <= 30 ? "Valide" : "Intvalid";
+            Console.WriteLine(massage);
+            Divider();
+            try
             {
-                Console.WriteLine("registeration");
+                Console.WriteLine("pleas enter your phoneNumber:");
+                string phonSTR = Console.ReadLine();
+                int phon = Convert.ToInt32(phonSTR);
+
             }
-            else
+            catch
             {
-                Console.WriteLine("reject");
+                Console.WriteLine("your number Invalid");
+                throw;
             }
             Divider();
 
-            Console.WriteLine("Hellocsharp!");
-            Divider();
-            Console.WriteLine("please enter your gender? (m,F)");
-            string genderSTR2 = Console.ReadLine();
-            Console.WriteLine("pleas enter your age");
-            string ageSTR2 = Console.ReadLine();
-            int age2 = Convert.ToInt32(ageSTR);
 
-            bool gender = default;
-            gender = genderSTR == "F" && age <=30? true : false;    
-            
-          
-            
+
+
+
+
+
+
+
+
+
 
         }
     }
