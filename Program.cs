20152ed6f4f4e173e5d7cb01ba2266e0da189801
Fatemehghasemi66 <f_s_ghasemi@yahoo.com>
2023-11-05 17:21:49 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace project2
+{
+    internal class Program
+    {
+        static void Divider()
+        {
+            Console.WriteLine("************************************************************");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellocsharp!");
+            Divider();
+            Console.WriteLine("please enter your gender? (m,F)");
+            string genderSTR = Console.ReadLine();
+            Console.WriteLine("pleas enter your age");
+            string ageSTR= Console.ReadLine();
+            int age = Convert.ToInt32(ageSTR);
+            if (genderSTR == "F" &&  age <= 30) 
+            {
+                Console.WriteLine("registeration");
+            }
+            else
+            {
+                Console.WriteLine("reject");
+            }
+
+            bool gender = default;
+            gender = genderSTR == "F" && age <=30? true : false;    
+            
+          
+            
+
+        }
+    }
+}
